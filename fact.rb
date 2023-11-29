c65c4fdaea5ca385f04ddf4e36f724a5bbcb0031
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-30 05:43:35 +0800

    improvements for ruby implementation

--- fact.rb
+++ fact.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 
+DEFAULT_N = 25
+
 def fact(n)
  return n * fact(n - 1) if n > 1
  return 1
 end
 
-puts fact(5)
+n = ARGV.first&.to_i || DEFAULT_N
+
+puts "fact(#{i}) = #{fact(i)}"
