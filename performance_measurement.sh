0dc483a5ef652d9ba69a2831d6fe036f7ce098b6
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-11-30 05:28:31 +0800

    benchmarking script

--- performance_measurement.sh
+++ performance_measurement.sh
@@ -2,17 +2,16 @@
 
 EXECUTABLES=`ls -la | grep "\-rwxr\-xr\-x" | awk '{print $9}'`
 
-echo -e "$EXECUTABLE"
-
 IFS="\n" read -ra a <<<$EXECUTABLES; declare -p a;
 
 string="$EXECUTABLES"
 set -f                      # avoid globbing (expansion of *).
 array=(${string/\s/})
-for i in "${!array[@]}"
-do
+for i in ${!array[@]}
+do  
   executable="${array[i]}"
   if [ -x "$executable" ]; then echo "$executable: `time ./$executable`"; fi
 done
 
 echo -e "Java: ` time java Fact`"
+echo -e "NodeJS: ` time node fact.js`"
